122d76e079ff4c52f1b50b94be1b9c36721fe6a4
salmasiddik <salma.siddik.usa@gmail.com>
2019-11-03 11:30:43 -0500

    Add chromedriver.exe and add resource folder, update test case

--- src/test/java/gmail/createaccount/Create_An_Accoun_In_Gmail.java
+++ src/test/java/gmail/createaccount/Create_An_Accoun_In_Gmail.java
@@ -1,9 +1,12 @@
 package gmail.createaccount;
 
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Create_An_Accoun_In_Gmail {
@@ -16,7 +19,7 @@ public class Create_An_Accoun_In_Gmail {
 		
 		
 		//System.setProperty("webdriver.chrome.driver", "/Users/ag9068/seleniumDemo/gmail/src/test/driver/chromedriver");
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\driver\\chromedriver.exe");
 		
 		//System.out.println(System.getProperty("user.dir"));
 		
@@ -32,9 +35,12 @@ public class Create_An_Accoun_In_Gmail {
 		driver.get("http://newtours.demoaut.com/");
 		
 		
+		//List<WebElement> links=driver.findElements(By.xpath("//a"));
+
 		
+		List<WebElement> link=driver.findElements(By.xpath("//button"));
 		
-		
+		System.out.println(link.size());
 		
 		
 		
@@ -45,13 +51,30 @@ public class Create_An_Accoun_In_Gmail {
 		
 		driver.findElement(By.xpath("//a[text()='REGISTER']")).click();
 
-		boolean isDisplay=driver.findElement(By.xpath("//img[@src='/images/masts/mast_register.gif']")).isDisplayed();
+/*		boolean isDisplay=driver.findElement(By.xpath("//img[@src='/images/masts/mast_register.gif']")).isDisplayed();
 
 		if(isDisplay==true) {
 			System.out.println("Register page open!!!");
 		}else {
 			System.out.println("Register page doesn't displayed!!!");
+		} */
+		//option-1
+		if(driver.findElements(By.xpath("//img[@src='/images/masts/mast_register.gif']")).size()>0) {
+			System.out.println("Register page open!!!");
+		}else {
+			System.out.println("Register page doesn't displayed!!!");
 		}
+		
+		
+		//option 2
+	/*	List<WebElement> imgs=driver.findElements(By.xpath("//img[@src='/images/masts/mast_register.gif']"));
+		
+		
+		if(imgs.size()>0) {
+			System.out.println("Register page open!!!");
+		}else {
+			System.out.println("Register page doesn't displayed!!!");
+		} */
 		
 		driver.findElement(By.name("firstName")).sendKeys("Salama");
 		
@@ -73,7 +96,7 @@ public class Create_An_Accoun_In_Gmail {
 		driver.findElement(By.name("confirmPassword")).sendKeys("test4321");
 		
 		
-		driver.findElement(By.name("register")).click();
+	//	driver.findElement(By.name("register")).click();
 
 	}
 
