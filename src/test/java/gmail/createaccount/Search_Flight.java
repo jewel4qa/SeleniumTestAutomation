301c1063f55a81629685d0450d86142dbb816730
ag9068 <ag9068@qnet.qualnet.org>
2019-11-13 22:13:16 -0500

    Add method

--- src/test/java/gmail/createaccount/Search_Flight.java
+++ src/test/java/gmail/createaccount/Search_Flight.java
@@ -15,8 +15,13 @@ public class Search_Flight {
 		SeleniumWebDriver.type(By.name("userName"), "salma11");
 		SeleniumWebDriver.type(By.name("password"), "test4321");
 		SeleniumWebDriver.click(By.name("login"));
-	
 		
+		SeleniumWebDriver.selctUsingValue(By.name("passCount"), "2");
+	
+		SeleniumWebDriver.selctUsingValue(By.name("fromPort"), "London");
+		SeleniumWebDriver.selctVisibleText(By.name("toMonth"), "December");
+		
+/*		
 		//Select by index
 	WebElement passCnt = SeleniumWebDriver.driver.findElement(By.name("passCount"));
 	Select select =new Select(passCnt);
@@ -30,16 +35,27 @@ public class Search_Flight {
 	
 	WebElement fromMonth=SeleniumWebDriver.driver.findElement(By.name("fromMonth"));
 	
+	Select fMonth=new Select(fromMonth);
+	fMonth.selectByVisibleText("March");
+	
+	
+	Select fMonth=new Select(fromMonth);
+	
 	//List<WebElement> options =fromMonth.findElements(By.tagName("option"));
 	List<WebElement> options =fromMonth.findElements(By.xpath(".//option"));
-	
+	String currText=null;
 	for(WebElement opt:options) {
-		System.out.println(opt.getText());
+		if(opt.getText().trim().equalsIgnoreCase("July")) {
+			currText=opt.getText();
+			break;
+		}
 	}
-	
-	
-	//Select fMonth=new Select(fromPort);
-	//fMonth.selectByVisibleText("December\n");
+	if(currText!=null) {
+		fMonth.selectByVisibleText(currText);
+	}else {
+		System.out.println("value doesn't exists!!!");
+	}
+*/
 	
 		
 	}
--- src/test/java/org/selenium/SeleniumWebDriver.java
+++ src/test/java/org/selenium/SeleniumWebDriver.java
@@ -8,20 +8,22 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumWebDriver {
 	public static WebDriver driver;
-	
-	
-	
+
+
+
 	public static void openBrowser(String browserName,String appUrl) {
-		
+
 		if(browserName.equalsIgnoreCase("chrome")) {
 			if(System.getProperty("os.name").toLowerCase().contains("mac")) {
-//				System.setProperty("webdriver.chrome.driver", "/Users/ag9068/seleniumDemo/gmail/src/test/driver/chromedriver");
+				System.setProperty("webdriver.chrome.driver", "/Users/ag9068/seleniumDemo/gmail/src/test/driver/chromedriver");
 			}else {
-				 System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\driver\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\driver\\chromedriver.exe");
 			}
 			driver=new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -33,7 +35,7 @@ public class SeleniumWebDriver {
 			driver.get(appUrl);
 		}else if(browserName.equalsIgnoreCase("edge")) {
 			System.setProperty("webdriver.chrome.driver", "/Users/ag9068/seleniumDemo/gmail/src/test/driver/geckodriver");
-			driver=new FirefoxDriver();
+			driver=new EdgeDriver();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.get(appUrl);
 		}else {
@@ -42,10 +44,12 @@ public class SeleniumWebDriver {
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.get(appUrl);
 		}
+		
+		
 	}
-	
 
-	
+
+
 	public static void type(final By locator, String valueToType) {		
 		List<WebElement> element = driver.findElements(locator);
 		if(element.size()>0) {
@@ -55,51 +59,169 @@ public class SeleniumWebDriver {
 			}else {
 				System.out.println(locator+" disabled!!");
 			}
-			
+
 		}else {
 			System.out.println(locator + " doesn't exists!!");
 		}
+
+	}
+
+	/**
+	 * click: method click on any given location.
+	 * @param locator Expect By.locatorName("locatorValue")..
+	 * @return void
+	 * @author ag9068
+	 */
+	public static void click(By locator) {
+		List<WebElement> element = driver.findElements(locator);
+		if(element.size()>0) {
+			if(driver.findElement(locator).isEnabled()) {
+				driver.findElement(locator).click();
+				System.out.println(locator + " clicked Successfully!!");
+			}else {
+				System.out.println(locator +" disable!!");
+			}
+
+		}else {
+			System.out.println(locator + " doesn't exists!!");
+		}
+	}
+
+
+	public static void clickWithJS(By locator) {
+		List<WebElement> elements = driver.findElements(locator);
+		if(elements.size()>0) {
+			WebElement element=driver.findElement(locator);
+			if(element.isEnabled()) {
+
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				js.executeScript("arguments[0].click();", element);
+				System.out.println(locator + " clicked Successfully!!");
+			}else {
+				System.out.println(locator +" disable!!");
+			}
+
+		}else {
+			System.out.println(locator + " doesn't exists!!");
+		}
+	}
+
+	public static void selctUsingIndex(By locator,int indexValue) {
+
+		List<WebElement> elements = driver.findElements(locator);
+		if(elements.size()>0) {
+			WebElement element = driver.findElement(locator);
+			if(element.isEnabled()) {
+				try {
+					Select select=new Select (element);
+					select.selectByIndex(indexValue);
+					System.out.println("index " + indexValue+ " item selected successfully!!");
+				} catch (Exception e) {
+					System.out.println("index " + indexValue + " doesn't available");
+					e.getMessage();
+					//e.printStackTrace();
+				}
+			}else {
+				System.out.println(locator + " disable!!!");
+			}
+			
+		}else {
+			System.out.println(locator + " doesn't exists!!!");
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param locator
+	 * @param itemName
+	 */
+	public static void selctUsingValue(By locator,String itemName) {
+
+		List<WebElement> elements = driver.findElements(locator);
+		if(elements.size()>0) {
+			WebElement element = driver.findElement(locator);
+			if(element.isEnabled()) {
+				try {
+					Select select=new Select (element);
+					select.selectByValue(itemName);
+					System.out.println( itemName+ " selected successfully!!");
+				} catch (Exception e) {
+					System.out.println(itemName + " doesn't available");
+				}
+			}else {
+				System.out.println(locator + " disable!!!");
+			}
+			
+		}else {
+			System.out.println(locator + " doesn't exists!!!");
+		}
+
+	}
+
+	/**
+	 * selctVisibleText method select item from the given location by text value
+	 * @param locator expect By.locatorName(locatorValue)
+	 * @param itemText visiable text 
+	 */
+	
+	public static void selctVisibleText(By locator, String itemText) {
+		
+		List<WebElement> elements = driver.findElements(locator);
+		if(elements.size()>0) {
+			WebElement element = driver.findElement(locator);
+			Select select = new Select (element);
+			if(element.isEnabled()) {
+				List<WebElement> options = element.findElements(By.tagName("option"));
+				try {
+					for(WebElement opt:options) {
+						if(opt.getText().trim().equalsIgnoreCase(itemText)) {
+							select.selectByVisibleText(opt.getText());
+							System.out.println(itemText + " Selected successfully!!");
+							break;
+						}
+					}
+				} catch (Exception e) {
+					System.out.println(itemText + " not available in the list");
+				}
+				
+/*				boolean bFound=false;
+				String curText=null;
+				for(WebElement opt:options) {
+					if(opt.getText().trim().equalsIgnoreCase(itemText)) {
+						bFound=true;
+						curText=opt.getText();
+						break;
+					}
+				}
+				
+				if(bFound==true) {
+					select.deselectByVisibleText(curText);
+					System.out.println(itemText + " selected successfully!!");
+				}else {
+					System.out.println(itemText + " not available in the list");
+				}*/
+				
+								
+			}else {
+				System.out.println(locator + " diable!!");
+			}
+			
+			
+		}else 
+			System.out.println(locator + "doesn't exists!!");
+		
 		
 	}
 	
-  public static void click(By locator) {
-	  List<WebElement> element = driver.findElements(locator);
-	  if(element.size()>0) {
-		  if(driver.findElement(locator).isEnabled()) {
-			  driver.findElement(locator).click();
-			  System.out.println(locator + " clicked Successfully!!");
-		  }else {
-			  System.out.println(locator +" disable!!");
-		  }
-		  
-	  }else {
-		  System.out.println(locator + " doesn't exists!!");
-	  }
-  }
-  
-  
-  public static void clickWithJS(By locator) {
-	  List<WebElement> elements = driver.findElements(locator);
-	  if(elements.size()>0) {
-		  WebElement element=driver.findElement(locator);
-		  if(element.isEnabled()) {
-			  
-			  JavascriptExecutor js = (JavascriptExecutor) driver;
-			  try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			  js.executeScript("arguments[0].click();", element);
-			  System.out.println(locator + " clicked Successfully!!");
-		  }else {
-			  System.out.println(locator +" disable!!");
-		  }
-		  
-	  }else {
-		  System.out.println(locator + " doesn't exists!!");
-	  }
-  }
-
+	
+	
+	
+	
 }
