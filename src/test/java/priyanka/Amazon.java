fc22388b69b6a11ad536c141eea5d9ed9f88e01d
Md G khandaker <PB>
2019-11-20 20:47:40 -0500

    PB

--- src/test/java/priyanka/Amazon.java
+++ src/test/java/priyanka/Amazon.java
@@ -8,8 +8,9 @@ public class Amazon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+
 		SeleniumWebDriver.openBrowser("chrome", "https://www.amazon.com");
+		SeleniumWebDriver.driver.manage().window().maximize();
 		//SeleniumWebDriver.click(By.xpath("//div[contains(text(),'Clothing,')]"));
 		//SeleniumWebDriver.click(By.xpath("//a[@href='/gp/browse.html?node=7147440011&ref_=nav_em_T1_0_4_12_2__sft_women']"));
 
@@ -20,15 +21,15 @@ public class Amazon {
 		if (isDisplayed >0) {
 			SeleniumWebDriver.click(By.xpath("//ul[@aria-labelledby='p_90-title']//input[@type='checkbox']"));
 		}else {System.out.println("Not There Yet");
-		
-		}
-		
-		
 
-		
-		
-		
-		
+		}
+
+
+
+
+
+
+
 	}
 
 }
