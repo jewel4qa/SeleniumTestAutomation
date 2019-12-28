ad34743f8c4122f7aa36de6c5c26763434af69dd
salmasiddik <salma.siddik.usa@gmail.com>
2019-12-28 12:04:38 -0500

    did the work

---
+++ src/test/java/salma/NewDriver.java
@@ -0,0 +1,18 @@
+package salma;
+
+public class NewDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+       WebDriver.drE	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/test/java/salma/ReadExcelFile1.java
@@ -0,0 +1,22 @@
+package salma;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ReadExcelFile1 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+         File file=new File("/users/ag9068/Documents/InportantInfo.xlsx");
+		
+        FileInputStream fis=new FileInputStream(file);
+		
+		
+		
+		
+		
+		
+	}
+
+}
