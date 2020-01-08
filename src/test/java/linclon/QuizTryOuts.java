6aa9d4a3eabf42c8726d632d607f22c912f89345
ahlincoln <links_habib@yahoo.com>
2020-01-08 18:56:09 -0500

    lk

--- src/test/java/linclon/QuizTryOuts.java
+++ src/test/java/linclon/QuizTryOuts.java
@@ -1,5 +1,6 @@
 package linclon;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.selenium.SeleniumWebDriver;
 
@@ -8,7 +9,12 @@ public class QuizTryOuts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		SeleniumWebDriver.openBrowser("chrome", "http://www.newtours.demoaut.com/");
+		
+		System.setProperty("Chrome", "/gmail/src/test/driver/chromedriver.exe");
+		WebDriver Driver = new chromedriver; 
+		
+		
+		/* SeleniumWebDriver.openBrowser("chrome", "http://www.newtours.demoaut.com/");
 
 Actions act = new Actions(SeleniumWebDriver); // where driver is WebDriver type
  
@@ -17,9 +23,17 @@ Actions act = new Actions(SeleniumWebDriver); // where driver is WebDriver type
  act.contextClick().perform();
 		
 		
+		*/
 		
-		
-		
+		Alert alt = driver.switchTo().alert(); // to move control to alert popup
+		 
+		 alt.accept(); // to click on ok.
+		 
+		 alt.dismiss(); // to click on cancel.
+		 
+		 //Then move the control back to main web page-
+		 
+		 driver.switchTo().window(mainPage);
 		
 		
 		
